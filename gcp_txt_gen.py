d9c10999720653bd308492b8517c3dff4db4592b
wolkstein <m.wolkstein@gmx.de>
2016-09-04 03:02:34 +0200

    add new function QR-Code Template compare, add new argument --first_gcp_point GCP Point start from [n] number, add argument --ignore_border to ignore template matches near image border [px].

--- gcp_txt_gen.py
+++ gcp_txt_gen.py
@@ -2,7 +2,12 @@ import numpy as np
 import cv2
 import argparse
 import glob
+from commands import getstatusoutput
+import os
+import errno
 
+
+# --qr_code_compare need "zbar-tools"
 
 # ---Helper---
 def roundTraditional(val,digits):
@@ -11,10 +16,19 @@ def roundTraditional(val,digits):
 
 GCP_DEBUGMODE = False
 MIN_MATCH_COUNT = 12
+USE_QRCODE = False
+
+FIRST_GCP_POINT = 1
+
+IGNORE_BORDER = 60
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action='store_true', required=False , help='Will additionally create an DEBUG-GCP_LIST.txt file with Pointnames P1 - Pn in each row')
+parser.add_argument('-q', '--qr_code_compare', action='store_true', required=False , help='compare QR-Code in GCP matches against the QR Code from Template QR-Code based GCP. This needs QR Code based GCPs')
 parser.add_argument('--min_match_count', type=int, default=12, required=False, help='min_match_count "Default = 12, Min = 4" set the minimum amount of required good keypoints to accept a match of Template in Image')
+parser.add_argument('-f', '--first_gcp_point', type=int, default=1, required=False, help='GCP Point start from [n] number')
+parser.add_argument('-i', '--ignore_border', type=int, default=60, required=False, help='Ignore GCP Point inside Image Border [px].')
+
 args = parser.parse_args()
 
 MIN_MATCH_COUNT = args.min_match_count
@@ -25,12 +39,30 @@ if MIN_MATCH_COUNT < 4:
 GCP_DEBUGMODE = args.debug
 if GCP_DEBUGMODE:
     print 'GCP_LIST in debugmode is not usable by ODM.'
+    
+USE_QRCODE = args.qr_code_compare
 
-#quit()
+QRCHECK_DIR = 'qr-checks'
+if USE_QRCODE:
+    print 'QR-Code Mode'
+    try:
+        os.makedirs(QRCHECK_DIR)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    testzbarimg = getstatusoutput("zbarimg --version")
+    if testzbarimg[0] == 32512:
+        print "zbarimg not found! Please install zbar-tools"
+        quit()
+    
+FIRST_GCP_POINT = args.first_gcp_point
+
+IGNORE_BORDER = args.ignore_border
+
 
 # ---Configure---
 # input Kordinaten File
-COORDSTXT = 'koords.txt'
+COORDSTXT = 'coords.txt'
 # output GCP_LIST_FILE
 GCP_LIST_FILE = 'gcp_list.txt'
 GCP_LIST_FILE_DBUG = 'debug-gcp_list.txt'
@@ -127,7 +159,7 @@ for i in range(len(imagePathList)):
         centroidXY = [0,0]
         
         paircord=[]
-        paircord.append("P%d%s" % (u+1,":"))
+        paircord.append("P%d%s" % (u+FIRST_GCP_POINT,":"))
         localTamplateKp = templateKp[u]
         if len(good)>MIN_MATCH_COUNT:
             src_pts = np.float32([ localTamplateKp[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
@@ -138,12 +170,13 @@ for i in range(len(imagePathList)):
             h,w = templateImg[u].shape
             pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
             dst = cv2.perspectiveTransform(pts,M)
-                
+     
+     
             #print len(good)
-            #print dst
             #print dst.shape
             #print dst[0,0,0]
             #print dst[2,0,0]
+            
                 
             # we calculate only for rectangle templates.
             # x center = (x0 - x2)/2 + x0
@@ -166,8 +199,50 @@ for i in range(len(imagePathList)):
             else:
                 centroidXY[1] = (abs(dst[0,0,1] - dst[2,0,1]))/2 + dst[2,0,1]
                 
-            print "HIT, X=%d Y=%d" %(centroidXY[0],centroidXY[1])
-            paircord.append("%d %d"%(centroidXY[0],centroidXY[1]))
+                
+            #check for template hits near image borders
+            ih,iw = image.shape
+            if centroidXY[0] < IGNORE_BORDER or centroidXY[1] < IGNORE_BORDER or centroidXY[0] > iw - IGNORE_BORDER or centroidXY[1] > ih - IGNORE_BORDER:
+                print "!!--------------Template near image boarde, we skip this Match"
+                paircord.append("F")
+                continue
+                
+                
+            # QRCODE Check
+            if USE_QRCODE:
+                # qrcode test                   | oben links                                 |
+                print "corner UL = %d, %d" % (centroidXY[0] - (w/2),centroidXY[1] - (h/2) )
+                print "corner LR = %d, %d" % (centroidXY[0] + (w/2),centroidXY[1] + (h/2) )
+                
+                border = 30
+                olx = centroidXY[0] - (w/2) - border
+                oly = centroidXY[1] - (h/2) - border
+                ulx = centroidXY[0] + (w/2) + border
+                uly = centroidXY[1] + (h/2) + border
+                
+                cropped = image[oly:uly, olx:ulx]
+                tmpimgname = "%s/tmpImage-Point_%d_at_Pos_%d_%d_%d_%d.png" %(QRCHECK_DIR,u+FIRST_GCP_POINT,olx,oly,ulx,uly)
+                tmptemplateimagename = "%s/GCP%d.png" %(QRCHECK_DIR,u+FIRST_GCP_POINT)
+                cv2.imwrite(tmpimgname, cropped)
+                cv2.imwrite(tmptemplateimagename, templateImg[u])
+                
+                
+                #print "Compare QR Code in Template Nr.: %d against image %s" % (u+FIRST_GCP_POINT,imagePathList[i])
+                testQrCodeBild = getstatusoutput("zbarimg -q --raw %s" % (tmpimgname))
+                testQrCodeGCPTeplate = getstatusoutput("zbarimg -q --raw %s" % (tmptemplateimagename))
+                
+                
+                
+                print "Compare QR-Code from Image %s vs QR-Code from Template %s" %(testQrCodeBild[1],testQrCodeGCPTeplate[1])
+                if testQrCodeBild[1] == testQrCodeGCPTeplate[1]:
+                    print "HIT! QR CODE is TRUE :) against Position: X=%d, Y=%d" %(centroidXY[0],centroidXY[1])
+                    paircord.append("%d %d"%(centroidXY[0],centroidXY[1]))
+                else:
+                    print "QR-Code FAIL :("
+                    paircord.append("F")
+            else:
+                print "HIT, X=%d Y=%d" %(centroidXY[0],centroidXY[1])
+                paircord.append("%d %d"%(centroidXY[0],centroidXY[1]))
                     
         else:
             print "FAIL, Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT) 
@@ -175,7 +250,7 @@ for i in range(len(imagePathList)):
             
         
         imageInfo.append(paircord)
-        print "Find %d matches between P%d and Image %d" %(len(good),u+1,i)
+        print "Find %d matches between P%d and Image %d" %(len(good),u+FIRST_GCP_POINT,i)
         
     AllImageResults.append(imageInfo)
 
@@ -203,7 +278,7 @@ for i in range(len(AllImageResults)):
   
   
 for i in range(len(templateImg)):
-    searchstring = "P%d%s" % (i+1,":")
+    searchstring = "P%d%s" % (i+FIRST_GCP_POINT,":")
     for p in range(len(AllImageResults)):
         imageTempNameGetter = AllImageResults[p]
         imagename = imageTempNameGetter[0]
@@ -251,16 +326,16 @@ for row in sortbyPmatches:
         koordsTextPosCoords = koordsTextSlit[1]
         # print "compare koordsTextPosNr: %s, against internalresultsPosNr: %s" %(koordsTextPosNr, internalresultsPosNr)
         if koordsTextPosNr == internalresultsPosNr:
-            print "--good--"
-            print koordsTextPosNr
-            print internalresultsPosNr
+            #print "--good--"
+            #print koordsTextPosNr
+            #print internalresultsPosNr
             if GCP_DEBUGMODE:
-                print "debugstring"
+                #print "debugstring"
                 debugstring = "%s %s %s" % (internalresultsPosNr, koordsTextPosCoords, internalresultsPosCoords)
-                print debugstring
+                #print debugstring
                 debugmergedStrings.append(debugstring)
             
-            print "normal string"
+            #print "normal string"
             normalstring = "%s %s" % (koordsTextPosCoords, internalresultsPosCoords)
             mergedStrings.append(normalstring)
                 
